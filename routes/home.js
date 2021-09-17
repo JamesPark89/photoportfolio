bf5da8b6a16867d7171778efbaccd3f95d381255
JamesPark89 <87226158+JamesPark89@users.noreply.github.com>
2021-09-17 23:45:11 +1000

    First commit
    -Init the project

---
+++ routes/home.js
@@ -0,0 +1,20 @@
+// routes/home.js
+
+var express = require('express');
+var router = express.Router();
+
+// Home
+router.get('/', function(req, res){
+  res.render('home/about');
+});
+router.get('/photo', function(req, res){
+  res.render('home/photo');
+});
+router.get('/video', function(req, res){
+  res.render('home/video');
+});
+router.get('/contact', function(req, res){
+  res.render('home/contact');
+});
+
+module.exports = router;
