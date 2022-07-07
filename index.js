03d7dc9c4a0281fa47e659f685a2296f886b177b
James Park <87226158+JamesPark89@users.noreply.github.com>
2022-07-07 12:00:29 +1000

    update
    update

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var app = express();
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname+'/public'));
+var express = require('express')
+var app = express()
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'))
 
-app.use('/', require('./routes/home'));
+app.use('/', require('./routes/home'))
 
-var port = 3000;
-app.listen(port, function(){
-  console.log('server on! http://localhost:'+port);
-});
+var port = process.env.Port || 3000
+app.listen(port, function () {
+  console.log('server on! http://localhost:' + port)
+})
